bc3595f27b6d8f38b93a3f45a741ae9e16df9599
hnamitha1 <namitha.h@gmail.com>
2016-04-18 17:26:58 +0100

    challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
-
   it 'returns a balance' do
     expect(subject.balance).to eq 0
   end
 
+  it {is_expected.to respond_to(:top_up).with(1).argument}
+
+  it "can top up the balance" do
+    expect { subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
 end
