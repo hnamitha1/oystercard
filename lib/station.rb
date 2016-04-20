0fcc43d1b4d8def02c9831bb1b3be4882879184c
hnamitha1 <namitha.h@gmail.com>
2016-04-20 11:03:27 +0100

    Challenge 13

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,16 @@
 class Station
+  
+  #attr_reader :name,:zone
+  def initialize(station)
+    @station = station 
+  end   
+
+  def name
+  	"oldstreet"
+  end
+
+  def zone
+  	3
+  end   	
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,14 @@
 require 'station'
 
 describe Station do
+   subject(:station) {described_class.new(name: "oldstreet",zone: 3)} 
+   
+   it "expects name to equal oldstreet" do
+       expect(station.name).to eq("oldstreet")
+   end    
+
+   it "expects zone to eq 3" do
+       expect(station.zone).to eq(3)
+   end	
 
 end
